6200f46c7bd931047d39e1a63ed82390ef4717a9
daw2-perez23 <perezabellanalex@fpllefia.com>
2023-02-28 11:43:09 +0100

    Fin sección 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generarJwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response) => {
 
@@ -47,7 +48,59 @@ const login = async(req, res = response) => {
 }
 
 
+const googleSignIn = async( req, res = response ) => {
+
+    const { id_token } = req.body
+
+    try {
+
+        const { name, img, email } = await googleVerify( id_token )
+        
+        let usuario = await Usuario.findOne({ email })
+
+        if( !usuario ){
+            // Creación del usuario
+            const data = {
+                name,
+                email,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+            }
+
+            console.log(data);
+
+            usuario = new Usuario( data )
+            await usuario.save()
+        }
+
+        if ( !usuario.state ){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id )
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar - Google'
+        })
+    }
+
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
